79b91266a1ecfdfb1282526658e16810673a6f20
Zannatul Naim <zannatulnaim09@gmail.com>
2022-12-20 10:51:07 +0600

    Create leet_code_buy_and_sell_stock_I.cpp

---
+++ Greedy/leet_code_buy_and_sell_stock_I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int p1 = prices[0];
+
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i-1] > prices[i]) {
+                if(p1 < prices[i]) continue;
+                else p1 = prices[i];
+            } else if(prices[i-1] < prices[i]) {
+                int x = prices[i] - p1;
+                if(x > profit) profit = x;
+                else continue;
+            }
+        }
+        return profit;
+    }
+};
