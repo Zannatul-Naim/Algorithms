50c19cf609ea1fe4335e3c2307c423ff4ea0ec2a
Zannatul Naim <zannatulnaim09@gmail.com>
2023-02-10 16:15:15 +0600

    B. Parallelogram is Back

---
+++ Geometry/codeforces_749B.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define input cin >>
+#define output cout <<
+#define pb push_back
+
+void fastIO() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int gcd(int a, int b) {
+	return b==0? a : gcd(b, a%b);
+}
+
+int lcm(int a, int b) {
+	return (a * (b / gcd(a, b)));
+}
+
+bool isPrime(int n) {  // A number is Prime number or not.
+	if(n < 2) return false;
+	for(int i = 2; i*i <= n; i++) {
+		if(n % i == 0) return false;
+	}
+	return true;
+}
+
+int factorial(int n) {
+	return n * factorial(n-1);
+}
+
+void solve() {
+
+	int x1, y1, x2, y2, x3, y3;
+
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+	cout << 3 << endl;
+
+	cout << x1 + x2 - x3 << " " << y1 + y2 - y3 << endl;
+	cout << x1 - x2 + x3 << " " << y1 - y2 + y3 << endl;
+	cout << -x1 + x2 + x3 << " " << -y1 + y2 + y3 << endl;
+
+}
+
+int main()
+{
+	fastIO();
+
+	int t = 1;
+	// cin >> t;
+
+	while(t--) {
+	  solve();
+	}
+
+	return 0;
+}
