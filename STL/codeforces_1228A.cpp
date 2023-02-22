7ced255f537613148eaa1619e2f0952adf14b947
Zannatul Naim <zannatulnaim09@gmail.com>
2023-02-22 22:23:52 +0600

    Update codeforces_1228A.cpp

--- STL/codeforces_1228A.cpp
+++ STL/codeforces_1228A.cpp
@@ -10,41 +10,6 @@ void fastIO() {
 	cout.tie(0);
 }
 
-int gcd(int a, int b) {
-	return b==0? a : gcd(b, a%b);
-}
-
-int lcm(int a, int b) {
-	return (a * (b / gcd(a, b)));
-}
-
-bool isPrime(int n) {  // A number is Prime number or not.
-	if(n < 2) return false;
-	for(int i = 2; i*i <= n; i++) {
-		if(n % i == 0) return false;
-	}
-	return true;
-}
-
-long long int factorial(int n) {
-	return n * factorial(n-1);
-}
-
-ll pow(int a, int n) {
-	if(n == 0) return 1;
-	if(n == 1) return a;
-	ll t = pow(a, n/2);
-	return t*t*pow(a, n%2);
-}
-
-template<class T>
-void swap(T &a, T &b) {
-	T temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
-
 bool checkIsDistinct(int n) {
   int digit = log10(n) + 1;
   set<int>s;
@@ -75,14 +40,6 @@ int main()
 {
 	fastIO();
 
-	// freopen("input.txt", 'r');
-	// freopen("output.txt", 'w');
-
-// 	int t;
-// 	cin >> t;
-// 	while(t--) {
-// 		solve();
-// 	}
 	solve();
 
 	return 0;
