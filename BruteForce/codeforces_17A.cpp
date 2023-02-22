ac9b19659ca109678f251c4d1ee7b37f2fe28574
Zannatul Naim <zannatulnaim09@gmail.com>
2023-02-22 22:23:00 +0600

    Update codeforces_17A.cpp

--- BruteForce/codeforces_17A.cpp
+++ BruteForce/codeforces_17A.cpp
@@ -14,26 +14,6 @@ void fastIO() {
 	cout.tie(0);
 }
 
-int gcd(int a, int b) {
-	return b==0? a : gcd(b, a%b);
-}
-
-int lcm(int a, int b) {
-	return (a * (b / gcd(a, b)));
-}
-
-bool isPrime(int n) {  // A number is Prime number or not.
-	if(n < 2) return false;
-	for(int i = 2; i*i <= n; i++) {
-		if(n % i == 0) return false;
-	}
-	return true;
-}
-
-long long int factorial(int n) {
-	return n * factorial(n-1);
-}
-
 void solve() {
  
  bool visited[1005];
@@ -44,16 +24,12 @@ void solve() {
  
  vector < int > v(n+1);
  
- /////////// sieve part //////////////////////
- 
  for(int i = 2; i <= n; i++) {
    if(!visited[i]) {
      v.push_back(i);
      for(int j = i*i; j <= n; j += i) visited[j] = true;
    }
  }
- 
- /////////// end of sieve part //////////////////////
  
  int cnt = 0;
  for(int i = 0; i < v.size(); i++) {
