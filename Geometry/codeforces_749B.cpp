384bf86e365e6f8b02fb4fa5b69969c952e2d6ee
Zannatul Naim <zannatulnaim09@gmail.com>
2023-02-22 22:22:11 +0600

    Update codeforces_749B.cpp

--- Geometry/codeforces_749B.cpp
+++ Geometry/codeforces_749B.cpp
@@ -4,34 +4,11 @@ using namespace std;
 
 #define ll long long
 #define endl "\n"
-#define input cin >>
-#define output cout <<
-#define pb push_back
 
 void fastIO() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-}
-
-int gcd(int a, int b) {
-	return b==0? a : gcd(b, a%b);
-}
-
-int lcm(int a, int b) {
-	return (a * (b / gcd(a, b)));
-}
-
-bool isPrime(int n) {  // A number is Prime number or not.
-	if(n < 2) return false;
-	for(int i = 2; i*i <= n; i++) {
-		if(n % i == 0) return false;
-	}
-	return true;
-}
-
-int factorial(int n) {
-	return n * factorial(n-1);
 }
 
 void solve() {
