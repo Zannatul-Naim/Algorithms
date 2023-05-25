b58fec5c12ac32fefd48f3e4c1d4c9f50c7f9975
Zannatul Naim <zannatulnaim09@gmail.com>
2023-05-26 00:40:13 +0600

    Activity selection problem

--- Greedy/GFG_N_meetings_in_one_room.cpp
+++ Greedy/GFG_N_meetings_in_one_room.cpp
@@ -2,11 +2,8 @@
 class Solution
 {
     public:
-    //Function to find the maximum number of meetings that can
-    //be performed in a meeting room.
     int maxMeetings(int start[], int end[], int n)
     {
-        // Your code here
         vector < pair < int , int > > v;
         
         for(int i = 0; i < n; i++) {
@@ -25,3 +22,33 @@ class Solution
         return cnt;
     }
 };
+
+/* Another new solution */
+
+// class Solution
+// {
+//     public:
+//     static bool cmp(pair<int, int> a, pair<int, int> b) {
+//         return a.second < b.second;
+//     }
+//     int maxMeetings(int start[], int end[], int n)
+//     {
+//         vector < pair < int , int > > v;
+//         for(int i = 0; i < n; i++) {
+//             v.push_back({start[i], end[i]});
+//         }
+//         sort(v.begin(), v.end(), cmp);
+        
+//         int cnt = 1;
+//         int endpoint = v[0].second;
+        
+//         for(int i = 1; i < n; i++) {
+            
+//             if(v[i].first > endpoint) {
+//                 cnt++;
+//                 endpoint = v[i].second;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
