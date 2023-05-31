11ffade5f2470ecbb449cc24818a026eab8e9b3c
Zannatul Naim <zannatulnaim09@gmail.com>
2023-05-31 16:43:43 +0600

    Update leetcode_28_kmp.cpp

--- String/leetcode_28_kmp.cpp
+++ String/leetcode_28_kmp.cpp
@@ -1,4 +1,56 @@
 
+const int N = 1e4+1;
+int F[N];
+
+void prefix_table(string &P, int m) {
+    int i = 1, j = 0;
+
+    while(i < m) {
+        if(P[i] == P[j]) {
+            F[i] = j+1;
+            i++, j++;
+        } else if(j > 0) {
+            j = F[j-1];
+        } else {
+            F[i] = 0;
+            i++;
+        }
+    }
+}
+
+class Solution {
+public:
+    int strStr(string T, string P) {
+        
+        int n = T.size(), m = P.size();
+        F[0] = 0;
+        prefix_table(P, m);
+
+        int i = 0, j = 0;
+
+        while(i < n) {
+            if(T[i] == P[j]) {
+                if(j == m-1) {
+                    return i-j;
+                } else {
+                    i++, j++;
+                }
+            } else if(j > 0) {
+                j = F[j-1];
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
+/*
+
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -40,3 +92,5 @@ public:
         return -1;
     }
 };
+
+*/
