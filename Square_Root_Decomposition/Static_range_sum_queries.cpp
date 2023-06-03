3c1e7dfedc2a5ba9c8c6d810fa9999af7cadba69
Zannatul Naim <zannatulnaim09@gmail.com>
2023-06-03 14:45:26 +0600

    Update Static_range_sum_queries.cpp

--- Square_Root_Decomposition/Static_range_sum_queries.cpp
+++ Square_Root_Decomposition/Static_range_sum_queries.cpp
@@ -2,19 +2,6 @@
 
 using namespace std;
 
-#define ll long long
-#define endl "\n"
-#define in cin >>
-#define out cout <<
-#define pb push_back
-
-void fastIO() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-}
-
-
 void solve() {
 
 	ll n, q;
@@ -40,14 +27,7 @@ void solve() {
 
 int main()
 {
-	fastIO();
-
-	int t = 1;
-	// cin >> t;
-
-	while(t--) {
-	  solve();
-	}
-
+	
+	solve();
 	return 0;
 }
