786b93b5314087d8efa5b66d5e77a886ec5ab1e2
Zannatul Naim <zannatulnaim09@gmail.com>
2023-06-09 23:28:35 +0600

    Extra line removed + renamed

---
+++ STL/codeforces_903C.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<sstream>
+using namespace std;
+
+void solve() {
+  
+  int n;
+  cin >> n;
+  
+  map < int, int > mp;
+  
+  for(int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    mp[x]++;
+  }
+  
+  int mx = -1;
+  for(auto i : mp) {
+    mx = max(mx, i.second);
+  }
+  cout << mx << endl;
+  
+}
+
+int main()
+{
+	fastIO();
+	
+	solve();
+
+	return 0;
+}
