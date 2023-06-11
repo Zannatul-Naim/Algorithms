32cf7033c4493cf5f4abdffda2118773ba2c72d6
Zannatul Naim <zannatulnaim09@gmail.com>
2023-06-11 20:53:32 +0600

    Update Codeforces_855A.cpp

--- STL/Codeforces_855A.cpp
+++ STL/Codeforces_855A.cpp
@@ -4,13 +4,6 @@ using namespace std;
 #define ll long long
 #define endl "\n"
 
-void fastIO()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-}
-
 void solve()
 {
 	int n;
@@ -34,7 +27,6 @@ void solve()
 
 int main()
 {
-	fastIO();
 	solve();
 
 	return 0;
